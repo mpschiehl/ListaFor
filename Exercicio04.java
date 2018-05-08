57cbecd6987250f53fbf196805b38438f406ab47
Marcio <mpschiehl@outlook.com>
2018-05-08 09:10:39 -0300

    desenvolvimento

--- Exercicio04.java
+++ Exercicio04.java
@@ -2,14 +2,24 @@
 public class Exercicio04 {
 
 	public static void main(String[] args) {
-		String texto = "";
-		for(int i = 0 ;i <21;i++){
-			for(int j =0;j<21;j++){
-			if(i ==0){
-				texto += " * ";
-			}	
-			}texto = texto + "\n";
-		}System.out.println(texto);
+	String texto ="",vazio ="*", cheio= "";
+	
+	for(int i = 0 ;i <21;i++){
+		cheio += " *";
+	}
+	
+	
+	for(int i = 0 ;i <41;i++){
+		vazio += " ";
+	}vazio += "*";
+	
+	texto += cheio +"\n";
+	
+	for(int i = 0 ;i <19;i++){
+		texto += vazio +"\n";
+	}
+	texto += cheio;
+	System.out.println(texto);
 	}
 
 }
---
+++ Exercicio05.java
@@ -0,0 +1,9 @@
+
+public class Exercicio05 {
+
+	public static void main(String[] args) {
+		for
+
+	}
+
+}
